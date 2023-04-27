714e65b031bb3bae0883cb255efbc74dcec90b7e
William Roberson <billkeyshawn03@gmail.com>
2023-04-27 15:59:35 -0400

    good version

---
+++ exercises/hello-world-website/better-website/scripts/greetings.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function showGreetings() {
+    const messageDiv = document.getElementById("myMessage");
+ //   console.log(messageDiv);
+    messageDiv.innerHTML = "Hi there!";
+}
+
+showGreetings();
